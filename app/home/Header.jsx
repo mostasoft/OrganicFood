7626451cb69a76f4ee7ed7d67695e9523137cc48
mostakin <mostasoft@gmail.com>
2025-09-30 09:00:56 +0600

    Describe changes

--- app/home/Header.jsx
+++ app/home/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
     <nav className="w-full fixed top-0 left-0 bg-white shadow z-50" >
       <div className="bg-green-500 w-full flex px-5 justify-between h-8 items-center">
         <div className="flex gap-4 items-center">
-          <a href=""  className=" flex items-center text-sm text-white"><FaPhone/>+8801305009243</a>
+          <a href=""  className="hidden md:flex lg:flex items-center text-sm text-white"><FaPhone/>+8801305009243</a>
           <a className="flex items-center text-sm text-white" href="" ><CgMail size={21}/>OrganicFood@gamail.com</a>
           </div>
         <div className="flex gap-4 text-sm text-white">
